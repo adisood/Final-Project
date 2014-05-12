7ef23f1a3396200e4ae6654e6aec3a00a7792b67
adisood <adisood@yahoo.com>
2014-05-12 00:51:01 -0400

    update

--- db/migrate/20140512043438_create_penn_courses.rb
+++ db/migrate/20140512043438_create_penn_courses.rb
@@ -1,6 +1,8 @@
 class CreatePennCourses < ActiveRecord::Migration
   def change
-    create_table :penn_courses do |t|
+  drop_table :penn_courses
+  end  
+  create_table :penn_courses do |t|
       t.string :code
       t.string :title
       t.text :des
